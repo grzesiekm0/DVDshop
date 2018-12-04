bc7334d2f19b9c13d192bee9345d7fabfd825935
Grzegorz Macko <grzmacko@gmail.com>
2018-12-04 15:43:29 +0100

    Problem with AppTest and log4j have been fixed

--- src/main/java/pl/grz/dvdshop/dao/ConnectionManager.java
+++ src/main/java/pl/grz/dvdshop/dao/ConnectionManager.java
@@ -1,20 +1,30 @@
 package pl.grz.dvdshop.dao;
 
-import pl.grz.dvdshop.exception.PersistenceException;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Logger;
+import java.sql.Statement;
 
+import org.apache.log4j.Logger;
+import pl.grz.dvdshop.exception.PersistenceException;
+
+/**
+ * Componente responsável por abrir e encerrar a conexão com o banco de dados.
+ *
+ * @author YaW Tecnologia
+ */
 public class ConnectionManager {
-    //Information for connecting to HSQLDB database.
+
+    //Informacões para conexão com banco de dados HSQLDB.
     private static final String STR_DRIVER = "org.hsqldb.jdbcDriver";
-    private static final String DATABASE = "article";
+    private static final String DATABASE = "mercadoria";
     private static final String STR_CON = "jdbc:hsqldb:file:" + DATABASE;
     private static final String USER = "sa";
     private static final String PASSWORD = "";
+
     private static Logger log = Logger.getLogger(ConnectionManager.class);
+
 
     public static Connection getConnection() throws PersistenceException {
         Connection conn = null;
@@ -23,16 +33,28 @@ public class ConnectionManager {
             conn = DriverManager.getConnection(STR_CON, USER, PASSWORD);
             conn.setAutoCommit(false);
 
-            log.debug("Open the connection to the database!");
+            log.debug("Aberta a conexão com banco de dados!");
             return conn;
         } catch (ClassNotFoundException e) {
-            String errorMsg = "Driver (JDBC) not found";
+            String errorMsg = "Driver (JDBC) não encontrado";
             log.error(errorMsg, e);
             throw new PersistenceException(errorMsg, e);
         } catch (SQLException e) {
-            String errorMsg = "Error getting connection";
+            String errorMsg = "Erro ao obter a conexão";
             log.error(errorMsg, e);
             throw new PersistenceException(errorMsg, e);
         }
     }
+
+    public static void closeAll(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+                log.debug("Fechada a conexão com banco de dados!");
+            }
+        } catch (Exception e) {
+            log.error("Não foi possivel fechar a conexão com o banco de dados!",e);
+        }
+    }
 }
+
